ce6bb87114ddb936a25771774dc14e68126dfd88
V1ktoriiaAndr <andrusyk.viktoriia@chnu.edu.ua>
2026-02-10 10:09:23 +0200

    Update OP26_Lab2_Thread.cpp
    Added my lab 2

--- OP26_Lab2_Thread/OP26_Lab2_Thread.cpp
+++ OP26_Lab2_Thread/OP26_Lab2_Thread.cpp
@@ -1,20 +1,88 @@
-// OP26_Lab2_Thread.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <cmath>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+mutex printMutex;
+
+class Ant {
+public:
+    string name;
+    double x, y, speed;
+
+    Ant(string n, double px, double py, double s) : name(n), x(px), y(py), speed(s) {}
+    virtual ~Ant() {}
+    virtual void move(int steps) = 0;
+
+protected:
+    void log(int step, int total) {
+        lock_guard<mutex> lock(printMutex);
+        cout << name << " | Step " << step << "/" << total << " | Pos: (" << x << ", " << y << ")" << endl;
+    }
+};
+
+class Worker : public Ant {
+    double startX, startY;
+    bool returning = false;
+public:
+    Worker(string n, double px, double py, double s) : Ant(n, px, py, s), startX(px), startY(py) {}
+
+    void move(int steps) override {
+        for (int i = 1; i <= steps; ++i) {
+            log(i, steps);
+            double tx = returning ? startX : 0, ty = returning ? startY : 0;
+            double dx = tx - x, dy = ty - y;
+            double dist = sqrt(dx * dx + dy * dy);
+
+            if (dist <= speed) {
+                x = tx; y = ty;
+                returning = !returning;
+            }
+            else {
+                x += (dx / dist) * speed;
+                y += (dy / dist) * speed;
+            }
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+    }
+};
+
+class Warrior : public Ant {
+    double radius, centerX, centerY, angle = 0;
+public:
+    Warrior(string n, double px, double py, double s, double r)
+        : Ant(n, px + r, py, s), radius(r), centerX(px), centerY(py) {
+    }
+
+    void move(int steps) override {
+        for (int i = 1; i <= steps; ++i) {
+            log(i, steps);
+            angle += speed / radius;
+            x = centerX + radius * cos(angle);
+            y = centerY + radius * sin(angle);
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+    }
+};
+
+int main() {
+    vector<thread> army;
+    int steps = 20;
+
+    Worker w1("Worker_1", 10, 15, 2.0), w2("Worker_2", 25, 5, 2.5);
+    Warrior v1("Warrior_1", 30, 30, 1.5, 5.0), v2("Warrior_2", 0, 0, 1.0, 10.0);
+
+    army.emplace_back(&Worker::move, &w1, steps);
+    army.emplace_back(&Worker::move, &w2, steps);
+    army.emplace_back(&Warrior::move, &v1, steps);
+    army.emplace_back(&Warrior::move, &v2, steps);
+
+    for (auto& t : army) t.join();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
